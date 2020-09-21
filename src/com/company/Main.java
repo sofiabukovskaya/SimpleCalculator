10a52a66dd49e46966ec85b726852c05fbff566f
sofiabukovskaya <sofi454545@gmail.com>
2020-09-21 23:24:36 +0300

    Initial Commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.channels.ReadableByteChannel;
+
+public class Main {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("Element: " + i);
+        }
+    }
+}
